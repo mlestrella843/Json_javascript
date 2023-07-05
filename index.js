1fcb91e1fdcc41a352954685f7e3c66c8de09067
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-05 17:08:12 -0600

    Using fecth

--- index.js
+++ index.js
@@ -2,21 +2,33 @@
 
 //1. 
 
-let datosJson;
-let xhr = new XMLHttpRequest();
-xhr.open('GET',"persona.json",true);
-xhr.responseType = 'json';
+// let datosJson;
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET',"persona.json",true);
+// xhr.responseType = 'json';
 
-xhr.onload = function(){
-    if(xhr.status === 200){
-        datosJson = xhr.response;
-        let elementoTexto = document.getElementById("nombre");
+// xhr.onload = function(){
+//     if(xhr.status === 200){
+//         datosJson = xhr.response;
+//         let elementoTexto = document.getElementById("nombre");
+//         elementoTexto.textContent = datosJson.nombre;
+//     }
+//     else{
+//         //manejar error despues
+//     }  
+// }
+// //Al final fuera de la funcion onload, llamamos el metodo del objeto de transmision que es xhr
+// xhr.send();
+
+//2. Using Fetch
+
+    let datosJson;
+
+    fetch('persona.json')
+    .then(res => res.json())
+    .then((salida) => {
+        datosJson = salida;
+
+        let elementoTexto = document.getElementById('nombre');
         elementoTexto.textContent = datosJson.nombre;
-    }
-    else{
-        //manejar error despues
-    }  
-}
-//Al final fuera de la funcion onload, llamamos el metodo del objeto de transmision que es xhr
-xhr.send();
-
+    })
